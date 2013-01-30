99c1ef6aa29f27cf844c67d7f289d83ca44ae852
Jake Sendar <jakesendar@gmail.com>
2013-01-30 12:31:02 -0500

    dsfg

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -54,7 +54,7 @@ class MediaController < ApplicationController
       @tag = params[:tag]
       @media = Tag.find_all(@tag).paginate(:page => params[:page], :per_page => 12)
     else   
-      @auth = request.env["omniauth.auth"]
+      @auth = request.env["omniauth.auth"]["user_info"]
       @media = Medium.all_redis.paginate(:page => params[:page], :per_page => 12)
     end
   end
