efa6c137540ad5d166d552d7ee5e5e5aaa02608d
Jake Sendar <jakesendar@gmail.com>
2013-02-06 16:15:00 -0500

    infinite scroll

--- app/assets/javascripts/videos.js
+++ app/assets/javascripts/videos.js
@@ -15,9 +15,11 @@ function scrollLoad(){
 	if ($('.pagination').length){
 		$(window).scroll(function() {
 			url = $('.pagination .next_page').attr('href')
-	   		if(url && $(window).scrollTop() > $(document).height() - $(window).height()){
+	   		if(url && $(window).scrollTop() > $(document).height() - $(window).height() - 50){
+				$('.pagination').text("Fetching more products...");
 				$.getScript(url);
 				$(window).scroll();
+				return false;
 	   		}
 		});
 	}
