ceb4f0c7edb91732b73cf19f5fe36688d9151934
Jake Sendar <jakesendar@gmail.com>
2013-02-12 15:52:47 -0500

    js changes

--- app/assets/javascripts/videos.js
+++ app/assets/javascripts/videos.js
@@ -67,47 +67,69 @@ function keyNav(){
 	var first = true
 	var counter = -1
 	$(document.documentElement).keyup(function (event) {
-		var array = []
-		$('.media_parent').each(function(){
-			array.push($(this))
-		});
-		function SortByUploaded(a, b){
-			var aUploaded = a.data('uploaded');
-			var bUploaded = b.data('uploaded'); 
-			return ((aUploaded < bUploaded) ? -1 : ((aUploaded > bUploaded) ? 1 : 0));
-		}
-		var boxes = array.sort(SortByUploaded).reverse();
-
-		if (event.keyCode == 39) {
-			counter += 1
-		} else if (event.keyCode == 37) { 
-			counter -= 1
-		} else if (event.keyCode == 38){
-			counter -= numColumns()
+		var keyPressed = event.keyCode;
+		
+		if (keyPressed == 39 || keyPressed == 37 || keyPressed == 38 || keyPressed == 40 ){
 			
-		}else if (event.keyCode == 40){
-			counter += numColumns()
-		}
-		$('.selected').trigger('mouseout').removeClass('selected');
-		$(boxes[counter]).trigger('mouseover').addClass('selected');	
+			if(counter < -1){
+				counter = -1
+			};
+			
+			var array = []
+			
+			$('.media_parent').each(function(){
+				array.push($(this));
+			});
+			
+			function SortByUploaded(a, b){
+				var aUploaded = a.data('uploaded');
+				var bUploaded = b.data('uploaded'); 
+				return ((aUploaded < bUploaded) ? -1 : ((aUploaded > bUploaded) ? 1 : 0));
+			}
+			
+			var boxes = array.sort(SortByUploaded).reverse();
+			if(first){
+				counter +=1
+				first = false
+			}
+			else if (keyPressed == 39) {
+				counter += 1
+			} else if (keyPressed == 37) { 
+				counter -= 1
+			} else if (keyPressed == 38){
+				counter -= numColumns()
+			
+			}else if (keyPressed == 40){
+				counter += numColumns()
+			}
+			
+			$('.selected').trigger('mouseout').removeClass('selected');
+			$(boxes[counter]).trigger('mouseover').addClass('selected');
+		};	
 	});
 }
 
 function numColumns(){
 	var conWidth = $('#masonry-container').width();
-	if (conWidth >= 1750){
-		return 6
-	}else if (conWidth >= 1460 && conWidth < 1750){
-		return 5
-	}else if (conWidth >= 1170 && conWidth < 1460){
-		return  4
-	}else if(conWidth < 1170 && conWidth >= 880){
-		return  3
-	}else if (conWidth < 880 && conWidth > 590){
-		return 2
-	}else{
-		return 1
-	}	
+	var cols = Math.floor(conWidth/220);
+	
+	if(cols < 4){
+		cols = 4
+	}
+	return cols
+	// if (conWidth >= 1750){
+	// 		Math.floor(conWidth/220)
+	// 	}else if (conWidth >= 1460 && conWidth < 1750){
+	// 		return 5
+	// 	}else if (conWidth >= 1170 && conWidth < 1460){
+	// 		return  4
+	// 	}else if(conWidth < 1170 && conWidth >= 880){
+	// 		return  3
+	// 	}else if (conWidth < 880 && conWidth > 590){
+	// 		return 2
+	// 	}else{
+	// 		return 1
+	// 	}	
 }
 
 function deleteVideo(video_id){
@@ -118,9 +140,6 @@ function triggerFilter(tag){
 	var idValue = "T_"+Math.floor(Math.random()*1000000);
 	$('#tag-search').tokenInput("add", {id: idValue, name: tag});	
 }
-
-
-
 
 function tokenInput() {
 	$("#tag-search").tokenInput("/media/token_input", {
@@ -174,7 +193,7 @@ $(document).ready(function(){
 	disableThumbs();
 	mediaZoom();
 	notLoggedIn();
-	// keyNav();
+	keyNav();
 	pollRedis("false");
 	tokenInput();
 	$('.vertical-rule').css('height', $('.side-panel-div').height());
@@ -185,7 +204,7 @@ $(document).ajaxComplete(function(){
 	disableThumbs();
 	mediaZoom();
 	notLoggedIn();
-	// keyNav();
+	keyNav();
 })
 
 
